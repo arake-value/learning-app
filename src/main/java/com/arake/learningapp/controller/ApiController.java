30396e10e5e2378cbeeb55296f27ac4cb3db47eb
arake-value <arake@value-it.co.jp>
2026-03-12 14:51:09 +0900

    2026-3-12 SLF4J対応

--- src/main/java/com/arake/learningapp/controller/ApiController.java
+++ src/main/java/com/arake/learningapp/controller/ApiController.java
@@ -3,6 +3,8 @@ package com.arake.learningapp.controller;
 import com.arake.learningapp.service.ApiService;
 import com.arake.learningapp.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class ApiController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ApiController.class);
     private final ApiService as;
 
     @GetMapping("get")
     public ResponseEntity<UserEntity> getUser(@RequestParam String id){
 
+        logger.debug("パラメーター: {}", id);
         UserEntity user = as.getUser(id);
+        logger.debug("戻り値: {}", user);
         return ResponseEntity.ok(user);
 
     }
--- src/main/java/com/arake/learningapp/service/ApiService.java
+++ src/main/java/com/arake/learningapp/service/ApiService.java
@@ -4,18 +4,20 @@ package com.arake.learningapp.service;
 import com.arake.learningapp.mapper.ApiMapper;
 import com.arake.learningapp.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ApiService {
 
-
+    private static final Logger logger = LoggerFactory.getLogger(ApiService.class);
     private final ApiMapper am;
 
     //idが無かたらnullが返る
     public UserEntity getUser(String id){
-        System.out.println(am.getUser(id));
+        logger.debug("id: {}", id);
         return am.getUser(id);
     }
 
